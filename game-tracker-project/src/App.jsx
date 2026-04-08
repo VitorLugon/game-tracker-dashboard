d87d699250412cd9cb3039880a31d001d7f9be47
Vitor Lugon <vitorlugon@hotmail.com.br>
2026-04-07 21:05:33 -0300

    implementação da lógica de consumo da api de jogos

--- game-tracker-project/src/App.jsx
+++ game-tracker-project/src/App.jsx
@@ -1,8 +1,60 @@
+import { useEffect, useState } from 'react'
 import Search from './components/Search.jsx'
-import { useState } from 'react'
+import Spinner from './components/Spinner.jsx';
 
-function App() {
+const API_BASE_URL = 'https://api.rawg.io/api/games';
+
+const API_KEY = import.meta.env.VITE_RAWG_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+  }
+}
+
+const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [gameList, setGameList] = useState([]);
+  const[isLoading, setIsLoading] = useState(false);
+
+  const fetchGames = async()=> {
+    setIsLoading(true);
+    setErrorMessage('');
+
+    try{
+      const endpoint = `${API_BASE_URL}?key=${API_KEY}&ordering=-added`;
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if(!response.ok){
+        throw new Error('Failed to fetch games');
+      }
+
+      const data = await response.json();
+
+      if(data.response === 'False'){
+        setErrorMessage(data.Error || 'Failed to fetch games');
+        setGameList([]);
+        return;
+      }
+
+      setGameList(data.results || []);
+      console.log(data);
+    } catch (error) {
+      console.error(`Error fetching games: ${error}`);
+      setErrorMessage('Error fetching games. Please try again later.');
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+
+  useEffect(() => {
+    
+    fetchGames();
+  }, []);
+
 
   return (
     <main>
@@ -15,10 +67,26 @@ function App() {
           <h1>
             Seus próximos <span className='text-gradient'>Jogos</span> favoritos,
             filtrados pela comunidade</h1>
-        </header>
 
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-          <h1 className='text-white'>{searchTerm}</h1>
+        </header>
+
+        <section className='all-games'>
+            <h2 className='mt-[40px]'>Todos os Jogos</h2>
+
+            {isLoading ? (
+              <Spinner />
+            ) : errorMessage ? (
+              <p className='text-red-500'>{errorMessage}</p>
+            ): (
+              <ul>
+                {gameList.map((game)=> (
+                  <p key={game.id} className='text-white'>{game.name}</p>
+                ))}
+              </ul>
+            )}
+        </section>
+        
       </div>
     </main>
   )
